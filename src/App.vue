cb5944f4127a35860ecd41ed257cfeb39de2d4d4
Dat Chu <90941901+lak-glitch@users.noreply.github.com>
2022-05-03 13:14:48 +0700

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="container">
-    <router-view />
+    <router-view v-slot="{ Component, route }">
+      <transition name="fade" mode="out-in" appear>
+        <div :key="route.fullPath">
+          <component :is="Component" />
+        </div>  
+      </transition>
+    </router-view>
   </div>
 </template>
 
@@ -12,5 +18,19 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.fade-enter-from {
+  opacity: 0;
+  transform: translateX(100px);
+}
+.fade-enter-active {
+  transition: all 0.5s ease-in-out;
+}
+.fade-leave-to {
+  opacity: 0;
+  transform: translateX(-100px);
+}
+.fade-leave-active {
+   transition: all 0.5s ease-out;
+}
 </style>
